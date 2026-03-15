408e9c621965b565202adb90c2a92b4984af4739
agnus sk <skagnussk@gmail.com>
2026-03-15 13:06:47 +0530

    eks.yaml edited

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,15 +1,15 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.0.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
-  
+
   enable_cluster_creator_admin_permissions = true
-  
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
@@ -19,6 +19,21 @@ module "eks" {
       desired_size   = var.desired_capacity
       min_size       = 1
       max_size       = 3
+    }
+  }
+
+  access_entries = {
+    github_actions_user = {
+      principal_arn = "arn:aws:iam::039952648478:user/github-actions-user"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
     }
   }
 }
